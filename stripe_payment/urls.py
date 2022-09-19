7fbfadf3d8cce18d83782d8fab30bf2014ab4b3f
MrShirokow <mr.shirokow@gmail.com>
2022-09-19 19:23:28 +0500

    add a base for the views and templates

--- stripe_payment/urls.py
+++ stripe_payment/urls.py
@@ -1,1 +1,9 @@
 from django.urls import re_path
+
+from stripe_payment.views import buy, get_item
+
+
+urlpatterns = [
+    re_path(r'^buy/(?P<id>[0-9]*)/?$', buy, name='buy-item'),
+    re_path(r'^item/(?P<id>[0-9]*)/?$', get_item, name='get-item'),
+]
--- stripe_payment/views.py
+++ stripe_payment/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from stripe_payment.models.item import Item
+
+
+def buy(request, id):
+    item = get_object_or_404(Item, pk=id)
+    return render(request, 'index.html', {'id': id, 'name': item.name,
+                                          'description': item.description, 'price': item.price})
+
+
+def get_item(request, id):
+    item = get_object_or_404(Item, pk=id)
+    return render(request, 'index.html', {'id': id, 'name': item.name,
+                                          'description': item.description, 'price': item.price})
