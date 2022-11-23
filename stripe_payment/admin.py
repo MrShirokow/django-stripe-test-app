a15b5848056633b6a18b8a2ae000905b4fc90d8c
MrShirokow <mr.shirokow@gmail.com>
2022-11-23 19:18:44 +0500

    migrate and refactoring

--- stripe_payment/admin.py
+++ stripe_payment/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
 from stripe_payment.models.item import Item
+from stripe_payment.models.order import Order
 
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'description', 'price')
+    list_display_links = list_display
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', )
     list_display_links = list_display
--- stripe_payment/urls.py
+++ stripe_payment/urls.py
@@ -2,17 +2,21 @@ from django.urls import path, re_path
 
 from stripe_payment.views import (
     CheckoutSessionCreatingView,
-    ItemLandingPageView,
+    OrderLandingPageView,
+    # ItemLandingPageView,
     ItemsLandingPageView,
     CancelView,
     SuccessView,
+    create_order,
 )
 
 
 urlpatterns = [
     path('', ItemsLandingPageView.as_view(), name='items-page'),
-    re_path(r'^buy/(?P<pk>[0-9]*)/?$', CheckoutSessionCreatingView.as_view(), name='buy-item'),
-    re_path(r'^item/(?P<pk>[0-9]*)/?$', ItemLandingPageView.as_view(), name='item-page'),
+    re_path(r'^order/create/?$', create_order, name='create-order'),
+    re_path(r'^order/(?P<pk>[0-9]*)/?$', OrderLandingPageView.as_view(), name='order-page'),
+    re_path(r'^buy/(?P<pk>[0-9]*)/?$', CheckoutSessionCreatingView.as_view(), name='buy-items'),
+    # re_path(r'^item/(?P<pk>[0-9]*)/?$', ItemLandingPageView.as_view(), name='item-page'),
     re_path(r'^success/?$', SuccessView.as_view(), name='success-pay'),
-    re_path(r'^cancel/?$', CancelView.as_view(), name='cancel-pay')
+    re_path(r'^cancel/?$', CancelView.as_view(), name='cancel-pay'),
 ]
