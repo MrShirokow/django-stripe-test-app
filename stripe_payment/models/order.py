92ba46f2b7f3a5b8b1727f9610dee5bd66a80603
MrShirokow <mr.shirokow@gmail.com>
2022-11-23 20:06:44 +0500

    add "cost" field to Order

--- stripe_payment/models/order.py
+++ stripe_payment/models/order.py
@@ -4,7 +4,12 @@ from .item import Item
 
 
 class Order(models.Model):
+    cost = models.IntegerField(default=0)
     items = models.ManyToManyField(Item)
+
+    @property
+    def display_price(self):
+        return f'{self.cost / 100:.2f}'
 
     class Meta:
         verbose_name = 'order'
