4b5cfc94821c1875685880d590e43dc7bf640a67
MrShirokow <mr.shirokow@gmail.com>
2022-11-23 19:25:14 +0500

    add OrderLandingPageView and create_order

--- stripe_payment/views.py
+++ stripe_payment/views.py
@@ -1,15 +1,59 @@
+import json
 import stripe
 
+from django.urls import reverse
 from django.views import View
 from django.conf import settings
-from django.http import JsonResponse
+from django.core.mail import send_mail
 from django.views.generic import TemplateView
 from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from django.http import (
+    JsonResponse, 
+    HttpResponse, 
+    HttpResponseRedirect,
+)
+
 
 from stripe_payment.models.item import Item
+from stripe_payment.models.order import Order
 
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+class SuccessView(TemplateView):
+    template_name = "success.html"
+
+
+class CancelView(TemplateView):
+    template_name = "cancel.html"
+
+
+class OrderLandingPageView(TemplateView):
+    template_name = 'order.html'
+
+    def get_context_data(self, **kwargs):
+        order_id = self.kwargs['pk']
+        order = get_object_or_404(Order, pk=order_id)
+        context = super(OrderLandingPageView, self).get_context_data(**kwargs)
+        context.update({
+            'items': order.items.all(),
+            'STRIPE_PUBLIC_KEY': settings.STRIPE_PUBLIC_KEY
+        })
+        return context
+
+
+class ItemsLandingPageView(TemplateView):
+    template_name = 'items.html'
+
+    def get_context_data(self, **kwargs):
+        items = Item.objects.all()
+        context = super(ItemsLandingPageView, self).get_context_data(**kwargs)
+        context.update({
+            'items': items,
+        })
+        return context
 
 
 class CheckoutSessionCreatingView(View):
@@ -40,36 +84,9 @@ class CheckoutSessionCreatingView(View):
         return JsonResponse({'id': checkout_session.id})
 
 
-class ItemLandingPageView(TemplateView):
-    template_name = 'item.html'
-
-    def get_context_data(self, **kwargs):
-        item_id = self.kwargs['pk']
-        item = get_object_or_404(Item, pk=item_id)
-        context = super(ItemLandingPageView, self).get_context_data(**kwargs)
-        context.update({
-            'item': item,
-            'STRIPE_PUBLIC_KEY': settings.STRIPE_PUBLIC_KEY
-        })
-        return context
-
-
-class ItemsLandingPageView(TemplateView):
-    template_name = 'items.html'
-
-    def get_context_data(self, **kwargs):
-        items = Item.objects.all()
-        context = super(ItemsLandingPageView, self).get_context_data(**kwargs)
-        context.update({
-            'items': items,
-            'STRIPE_PUBLIC_KEY': settings.STRIPE_PUBLIC_KEY
-        })
-        return context
-
-
-class SuccessView(TemplateView):
-    template_name = "success.html"
-
-
-class CancelView(TemplateView):
-    template_name = "cancel.html"
+def create_order(request):
+    items = Item.objects.filter(id__in=request.POST.getlist('item')).all()
+    order = Order.objects.create()
+    order.items.set(items)
+    order.save()
+    return HttpResponseRedirect(reverse('order-page', kwargs={'pk': order.id}))
