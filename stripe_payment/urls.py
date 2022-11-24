c35d311edc98b023a91817e933acccd8c0a6c779
MrShirokow <mr.shirokow@gmail.com>
2022-11-25 01:31:59 +0500

    refactoring

--- stripe_payment/urls.py
+++ stripe_payment/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, re_path
 from stripe_payment.views import (
     CheckoutSessionCreatingView,
     OrderPageView,
-    # ItemLandingPageView,
     HomePageView,
     CancelView,
     SuccessView,
@@ -16,7 +15,6 @@ urlpatterns = [
     re_path(r'^order/create/?$', create_order, name='create-order'),
     re_path(r'^order/(?P<pk>[0-9]*)/?$', OrderPageView.as_view(), name='order-page'),
     re_path(r'^order/(?P<pk>[0-9]*)/buy/?$', CheckoutSessionCreatingView.as_view(), name='buy-items'),
-    # re_path(r'^item/(?P<pk>[0-9]*)/?$', ItemLandingPageView.as_view(), name='item-page'),
     re_path(r'^success/?$', SuccessView.as_view(), name='success-pay'),
     re_path(r'^cancel/?$', CancelView.as_view(), name='cancel-pay'),
 ]
