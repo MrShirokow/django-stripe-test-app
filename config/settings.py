87a36e87a0f9a5be61b9ce5426014835b0dd8ed4
MrShirokow <mr.shirokow@gmail.com>
2022-11-24 16:49:30 +0500

    create static

--- config/settings.py
+++ config/settings.py
@@ -137,8 +137,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_ULR = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'stripe_payment/static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'stripe_payment/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
+
 from stripe_payment import urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(urls.urlpatterns))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
